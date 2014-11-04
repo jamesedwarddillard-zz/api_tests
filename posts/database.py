2c933eb20bb0f0e631fde0b0cf0b99600ed11b29
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-11-03 16:43:23 -0800

    first commit

--- posts/database.py
+++ posts/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,3 +8,4 @@ engine = create_engine(app.config["DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -25,5 +25,172 @@ class TestAPI(unittest.TestCase):
         # Remove the tables and their data from the database
         Base.metadata.drop_all(engine)
 
+    def testGetEmptyPosts(self):
+        """ Getting posts from an empty database"""
+        response = self.client.get("/api/posts",
+            headers=[("Accept", "application/json")]
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(data, [])
+
+    def testGetPosts(self):
+        """ Getting posts from a populated database """
+        postA = models.Post(title="Title A", body=
+            "Posting up over heeeere")
+        postB = models.Post(title="Other title", body=
+            "What a wonderful world")
+        session.add_all([postA, postB])
+        session.commit()
+
+        response = self.client.get("/api/posts",
+            headers=[("Accept", "application/json")] 
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(len(data), 2)
+
+        postA = data[0]
+        self.assertEqual(postA["title"], "Title A")
+        self.assertEqual(postA["body"], "Posting up over heeeere")
+
+        postB = data[1]
+        self.assertEqual(postB["title"], "Other title")
+        self.assertEqual(postB["body"], "What a wonderful world")
+
+    def testGetPost(self):
+        """ Get a single post from a populated database """
+        postA = models.Post(title="Title A", body=
+            "Posting up over heeeere")
+        postB = models.Post(title="Other title", body=
+            "What a wonderful world")
+        session.add_all([postA, postB])
+        session.commit()
+
+        response = self.client.get("/api/posts/{}".format(postB.id),
+            headers=[("Accept", "application/json")]
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        post = json.loads(response.data)
+        self.assertEqual(post["title"], "Other title")
+        self.assertEqual(post["body"], "What a wonderful world")
+
+    def testGetNonExistentPost(self):
+        """ Getting a single post which doesn't exist """
+        response = self.client.get("/api/posts/1",
+            headers=[("Accept", "application/json")]
+            )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], "Could not find post with id 1")
+
+    def testUnsupportedAcceptHeader(self):
+        response = self.client.get("/api/posts", 
+            headers = [("Accept", "application/xml")]
+            )
+
+        self.assertEqual(response.status_code, 406)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], 
+            "Request must accept application/json data")
+
+    def testGetPostsWithTitle(self):
+        """ Filtering posts by title """
+        postA = models.Post(title="Post with green eggs", body="Just a test")
+        postB = models.Post(title="Post with ham", body="Still a test")
+        postC = models.Post(title="Post with green eggs and ham", body="Another test")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?title_like=ham",
+            headers=[("Accept", "application/json")]
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data)
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with ham")
+        self.assertEqual(post["body"], "Still a test")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Post with green eggs and ham")
+        self.assertEqual(post["body"], "Another test")
+
+    def testGetPostsWithBody(self):
+        """ Filtering posts by body """
+        postA = models.Post(title="Post with green eggs", body="Just a test")
+        postB = models.Post(title="Post with ham", body="Still a test")
+        postC = models.Post(title="Post with green eggs and ham", body="Still testing")
+        
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?body_like=still",
+            headers=[("Accept", "application/json")]
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data)
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with ham")
+        self.assertEqual(post["body"], "Still a test")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Post with green eggs and ham")
+        self.assertEqual(post["body"], "Still testing")
+
+    def testGetPostsWithTitleAndBody(self):
+        """ Filtering posts by title and body """
+        postA = models.Post(title="Green eggs and ham", body="A post by Sam I Am about my favorite foods")
+        postB = models.Post(title="Green fish blue fish", body="A post about Sam I Am's fish bowl")
+        postC = models.Post(title="Green is my favorite color", body="A post by James about how much I love the color green")
+        postD = models.Post(title="The Cat in the Hat", body="A post by Sam I Am about my greatest rival for power")
+
+        session.add_all([postA, postB, postC, postD])
+        session.commit()
+
+        response = self.client.get("/api/posts?title_like=green&body_like=Sam",
+            headers=[("Accept", "application/json")]
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data)
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Green eggs and ham")
+        self.assertEqual(post["body"], "A post by Sam I Am about my favorite foods")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Green fish blue fish")
+        self.assertEqual(post["body"], "A post about Sam I Am's fish bowl")
+
+
+
 if __name__ == "__main__":
     unittest.main()
