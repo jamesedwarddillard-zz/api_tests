9b09a9640381c32b85dd6bf1f536e1c601252b0a
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-11-03 16:36:44 -0800

    api

--- posts/api.py
+++ posts/api.py
@@ -8,3 +8,41 @@ import decorators
 from posts import app
 from database import session
 
+@app.route("/api/posts", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get():
+	""" Get a list of posts from the database """
+	#Get the querystring arguments
+	title_like = request.args.get("title_like")
+	body_like = request.args.get("body_like")
+
+	#Get and filter posts from the database
+	posts = session.query(models.Post)
+	if title_like:
+		posts = posts.filter(models.Post.title.contains(title_like))
+
+	if body_like:
+		posts = posts.filter(models.Post.body.contains(body_like))
+	posts = posts.all()
+
+	#Convert the posts to JSON and return a response
+	data = json.dumps([post.as_dictionary() for post in posts])
+	return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["GET"])
+@decorators.accept("application/json") 
+def post_get(id):
+	""" Single post endpoint """
+	#Get the post from the database
+	post = session.query(models.Post).get(id)
+
+	#Check if post exists; if not return 404 with error message
+
+	if not post:
+		message = "Could not find post with id {}".format(id)
+		data = json.dumps({"message": message})
+		return Response(data, 404, mimetype="application/json")
+
+	#Return the post as a JSON
+	data = json.dumps(post.as_dictionary())
+	return Response(data, 200, mimetype="application/json")
